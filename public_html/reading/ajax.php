676ebc4b3fd6495e67b6eb8b178412c909b2f08e
mikes <shilov.mike@gmail.com>
2025-04-14 10:51:22 +0400

    Revamp reading page and remove unused resources
    Replaced iframe-based reading page with an interactive form for generating a reading plan. Removed unused service worker (sw.js) and outdated location.txt. Updated page structure, styles, and scripts to enhance functionality and user experience.

--- public_html/reading/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: text/plain; charset=utf-8");
-$filename = 'location.txt';
-if (isset($_GET['set'])) {
-    file_put_contents ($filename, $_GET['set']);
-	//file_get_contents('https://somewhere/reading/ajax.php?set='.urlencode($_GET['set']));
-}
-else
-    echo file_get_contents ($filename);
